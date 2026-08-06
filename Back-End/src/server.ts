22585b6912476966f92a92ad1185ab74db17d036
Gustavo leal <lealgustavo0099@gmail.com>
2026-08-05 21:15:07 -0300

    deploy

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -40,7 +40,7 @@ server.register(AuthRoute)
 server.register(ProfileRoute)
 server.register(ProjectRoute)
 
-server.listen({ port: env.PORT }, (err, address) => {
+server.listen({ port: env.PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
